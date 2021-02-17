ae974aedf84f2802be88941d84c67e6f43987f9c
Bilyana Yordanova <sarcoholic98@gmail.com>
2021-02-17 23:57:06 +0200

    Fix timer issue

--- src/sidepanel.rs
+++ src/sidepanel.rs
@@ -1,9 +1,9 @@
 extern crate gtk;
+use crate::board::Board;
 use glib::Continue;
 use gtk::*;
 use std::cell::RefCell;
 use std::rc::Rc;
-use crate::board::Board;
 
 pub struct SidePanel {
     pub container: Box,
@@ -35,20 +35,28 @@ impl SidePanel {
 
         let bc = board.clone();
 
-        glib::timeout_add_seconds_local(
-            1,
-            move || {
-                let mut b = bc.borrow_mut();
-                if !b.game_over{
-                    let a = b.seconds_elapsed;
-                    let seconds = if a >= 10 {(a%60).to_string()} else {format!("0{a}", a = a).to_string()};
-                    let time_elapsed = format!("<span font-family='monospace'>{d}{m}:{s}</span>", d = a / 600, m = a / 60, s = seconds).to_string();
-                    label.set_markup(&time_elapsed);
-                    flags_label.set_label(format!("{}/10", b.flags_placed).as_str());
-                    b.seconds_elapsed += 1;
-                }
-                Continue(true)
-            });
+        glib::timeout_add_seconds_local(1, move || {
+            let mut b = bc.borrow_mut();
+            if !b.game_over {
+                let a = b.seconds_elapsed;
+                let seconds = if (a % 60) >= 10 {
+                    (a % 60).to_string()
+                } else {
+                    format!("0{}", a % 60).to_string()
+                };
+                let time_elapsed = format!(
+                    "<span font-family='monospace'>{d}{m}:{s}</span>",
+                    d = a / 600,
+                    m = a / 60,
+                    s = seconds
+                )
+                .to_string();
+                label.set_markup(&time_elapsed);
+                flags_label.set_label(format!("{}/10", b.flags_placed).as_str());
+                b.seconds_elapsed += 1;
+            }
+            Continue(true)
+        });
 
         SidePanel { container }
     }
