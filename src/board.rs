916540410cf4379ee3823913336a9a8d49aabaa7
Bilyana Yordanova <sarcoholic98@gmail.com>
2021-02-18 14:57:22 +0200

    Formatting

--- src/board.rs
+++ src/board.rs
@@ -7,7 +7,7 @@ use std::cell::RefCell;
 use std::collections::VecDeque;
 use std::rc::Rc;
 
-use crate::Field;
+use crate::field::Field;
 
 pub struct Adjacent {
     pub coord: (i8, i8),
@@ -16,8 +16,12 @@ pub struct Adjacent {
 }
 
 impl Adjacent {
-    pub fn new(dimension:i8, x: usize, y: usize) -> Adjacent {
-        Adjacent{coord:(x as i8, y as i8), offset: (-1, -1), dimension}
+    pub fn new(dimension: i8, x: usize, y: usize) -> Adjacent {
+        Adjacent {
+            coord: (x as i8, y as i8),
+            offset: (-1, -1),
+            dimension,
+        }
     }
 }
 
@@ -30,18 +34,20 @@ impl Iterator for Adjacent {
             adjacent = (self.coord.0 + self.offset.0, self.coord.1 + self.offset.1);
             if self.offset.0 < 1 && self.offset.1 <= 1 {
                 self.offset.0 += 1;
-                if self.offset == (0,0) {
+                if self.offset == (0, 0) {
                     self.offset.0 += 1;
                 }
-            }
-            else if self.offset.0 >= 1 && self.offset.1 <= 1 {
+            } else if self.offset.0 >= 1 && self.offset.1 <= 1 {
                 self.offset.0 = -1;
                 self.offset.1 += 1;
-            }
-            else {
+            } else {
                 return None;
             }
-            if adjacent.0 >= 0 && adjacent.0 < self.dimension && adjacent.1 >= 0 && adjacent.1 < self.dimension {
+            if adjacent.0 >= 0
+                && adjacent.0 < self.dimension
+                && adjacent.1 >= 0
+                && adjacent.1 < self.dimension
+            {
                 break;
             }
         }
@@ -104,55 +110,57 @@ impl Board {
                     //if more than there are mines are flagged, do nothing
                     //if less than there are mines are flagged, do nothing
                     //if adjacent field has a mine that is not flagged, explode and end game
-                        let mut mines_flagged = 0;
+                    let mut mines_flagged = 0;
+                    for adj in Adjacent::new(board.dimension, x, y) {
+                        mines_flagged += board.fields[adj.0][adj.1].is_flagged as i8;
+                    }
+                    if mines_flagged == value_on_button || value_on_button == 0 {
+                        //recursive reveal adjacent fields without bombs
+                        let mut fields_to_traverse = VecDeque::new();
+                        fields_to_traverse.push_back((x, y));
+                        //println!("mines_flagged: {}, value_on_field:{}", mines_flagged, value_on_button);
+
                         for adj in Adjacent::new(board.dimension, x, y) {
-                            mines_flagged += board.fields[adj.0][adj.1].is_flagged as i8;
-                        }
-                        if mines_flagged == value_on_button || value_on_button == 0 {
-                            //recursive reveal adjacent fields without bombs
-                            let mut fields_to_traverse = VecDeque::new();
-                            fields_to_traverse.push_back((x, y));
-                            //println!("mines_flagged: {}, value_on_field:{}", mines_flagged, value_on_button);
-
-                            for adj in Adjacent::new(board.dimension, x,y) {
-                                if !board.fields[adj.0][adj.1].is_clicked && !board.is_mine_on_field(adj.0, adj.1) {
-                                    fields_to_traverse.push_back(adj);
-                                    board.fields[adj.0][adj.1].is_clicked = true;
-                                }
-                                else if board.is_mine_on_field(adj.0, adj.1) && !board.fields[adj.0][adj.1].is_flagged{
-                                    board.reveal_all_mines();
-                                    println!("not flagged rigth bih");
-                                } 
+                            if !board.fields[adj.0][adj.1].is_clicked
+                                && !board.is_mine_on_field(adj.0, adj.1)
+                            {
+                                fields_to_traverse.push_back(adj);
+                                board.fields[adj.0][adj.1].is_clicked = true;
+                            } else if board.is_mine_on_field(adj.0, adj.1)
+                                && !board.fields[adj.0][adj.1].is_flagged
+                            {
+                                board.reveal_all_mines();
+                                println!("not flagged rigth bih");
                             }
+                        }
 
-                            while let Some((x, y)) = fields_to_traverse.pop_front() {
-                                let current_field_value = board.fields[x][y].value;
-                                board.fields[x][y].is_clicked = true;
+                        while let Some((x, y)) = fields_to_traverse.pop_front() {
+                            let current_field_value = board.fields[x][y].value;
+                            board.fields[x][y].is_clicked = true;
 
-                                if current_field_value == 0 {
-                                    for adj in Adjacent::new(board.dimension, x,y) {
-                                        if !board.fields[adj.0][adj.1].is_clicked {
-                                            fields_to_traverse.push_back(adj);
-                                            board.fields[adj.0][adj.1].is_clicked = true;
-                                        }
+                            if current_field_value == 0 {
+                                for adj in Adjacent::new(board.dimension, x, y) {
+                                    if !board.fields[adj.0][adj.1].is_clicked {
+                                        fields_to_traverse.push_back(adj);
+                                        board.fields[adj.0][adj.1].is_clicked = true;
                                     }
                                 }
-                                let pb = if board.fields[x][y].is_flagged {
-                                    board.pixbufs.last()
-                                }
-                                else {
-                                    Some(&board.pixbufs[(current_field_value + 1) as usize])
-                                };
-
-                                board.fields[x][y]
-                                    .button
-                                    .get_icon_widget()
-                                    .unwrap()
-                                    .downcast::<gtk::Image>()
-                                    .unwrap()
-                                    .set_from_pixbuf(pb);
                             }
+                            let pb = if board.fields[x][y].is_flagged {
+                                board.pixbufs.last()
+                            } else {
+                                Some(&board.pixbufs[(current_field_value + 1) as usize])
+                            };
+
+                            board.fields[x][y]
+                                .button
+                                .get_icon_widget()
+                                .unwrap()
+                                .downcast::<gtk::Image>()
+                                .unwrap()
+                                .set_from_pixbuf(pb);
                         }
+                    }
                 });
 
                 let board_clone = board_rc.clone();
@@ -163,15 +171,17 @@ impl Board {
                     .connect_button_press_event(move |button, event| {
                         let mut board = board_clone.borrow_mut();
 
-                        if event.get_button() == 3 && board.flags_placed < 10  && !board.fields[x][y].is_clicked{
+                        if event.get_button() == 3
+                            && board.flags_placed < 10
+                            && !board.fields[x][y].is_clicked
+                        {
                             let flag = board.fields[x][y].is_flagged;
                             //if unflag reduce count
                             board.flags_placed += -1 * flag as i8 + !flag as i8;
 
                             let pb = if flag {
                                 Some(&board.pixbufs[8 as usize])
-                            }
-                            else {
+                            } else {
                                 board.pixbufs.last()
                             };
 
@@ -240,8 +250,7 @@ impl Board {
         for i in 0..self.dimension {
             for j in 0..self.dimension {
                 if !self.is_mine_on_field(i as usize, j as usize) {
-                    let mut iter = Adjacent::new(self.dimension, i as usize, j as usize);
-                    for adj in iter {
+                    for adj in Adjacent::new(self.dimension, i as usize, j as usize) {
                         if self.is_mine_on_field(adj.0, adj.1) {
                             self.fields[i as usize][j as usize].value += 1;
                         }
@@ -272,10 +281,6 @@ impl Board {
         pixbufs
     }
 
-    fn is_valid_field(&mut self, x: i8, y: i8) -> bool {
-        x >= 0 && x < self.dimension as i8 && y >= 0 && y < self.dimension as i8
-    }
-
     fn is_mine_on_field(&mut self, x: usize, y: usize) -> bool {
         self.fields[x][y].value == -1
     }
@@ -304,6 +309,6 @@ mod tests {
     #[test]
     fn test_tests() {
         let mut adj = Adjacent::new(4, 1, 1);
-        assert_eq!(adj.next(), Some((0,0)));
+        assert_eq!(adj.next(), Some((0, 0)));
     }
 }
