6261b443dc30ccbec01dc2505583ed1a6731542e
Bilyana Yordanova <sarcoholic98@gmail.com>
2021-02-19 23:41:11 +0200

    Add struct for iterating fields

---
+++ src/field_gen.rs
@@ -0,0 +1,26 @@
+pub struct FieldGenerator {
+    pub counter: usize,
+    dimension: usize,
+}
+
+impl FieldGenerator {
+    pub fn new(dimension: i8) -> FieldGenerator {
+        FieldGenerator{counter: 0, dimension: dimension as usize}
+    }
+}
+
+impl Iterator for FieldGenerator {
+    type Item = (usize, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.counter == self.dimension * self.dimension {
+            return None;
+        }
+
+        let x = self.counter / self.dimension;
+        let y = self.counter % self.dimension;
+        self.counter += 1;
+
+        Some((x as usize, y as usize))
+    }
+}
